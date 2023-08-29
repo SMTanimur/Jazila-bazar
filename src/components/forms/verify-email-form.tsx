455a28c679ca962fc11d179a29c531110c00df54
MushfiqurRahmanTanim <smtanimurrahman404@gmail.com>
2023-08-29 18:16:23 +0600

    feat: :hammer: verify account

--- src/components/forms/verify-email-form.tsx
+++ src/components/forms/verify-email-form.tsx
@@ -2,39 +2,44 @@
 
 import * as React from 'react';
 
-
 import { Button } from '@/components/ui/button';
-import {
-  Form
-} from '@/components/ui/form';
+import { Form } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 
 import { Icons } from '../ui/icons';
 import { useAuth } from '@/hooks/api/auth/useAuth';
+import Link from 'next/link';
 
 type Props = {
   token: string;
 };
 
 export function VerifyEmailForm({ token }: Props) {
-  const { verifyForm, VerifyLoading, IsVerifyError, attemptToVerifyEmail } =
-    useAuth();
+  const { VerifyLoading, attemptToVerifyEmail, IsVerifyError } = useAuth();
+
+  React.useEffect(() => {
+    if (token) {
+      attemptToVerifyEmail({ token: token });
+    }
+  }, [token]);
   return (
-      <form
-        onSubmit={(...args) =>
-          attemptToVerifyEmail({ token: token })
-        }
-      >
-        <Button disabled={VerifyLoading}>
-          {VerifyLoading && (
-            <Icons.spinner
-              className='mr-2 h-4 w-4 animate-spin'
-              aria-hidden='true'
-            />
-          )}
-          Create account
-          <span className='sr-only'>Create account</span>
+    !IsVerifyError ? (
+    <Button disabled={VerifyLoading}>
+      {VerifyLoading ? (
+        <Icons.spinner
+          className='mr-2 h-4 w-4 text-green-400 animate-spin'
+          aria-hidden='true'
+        />
+      ) : (
+        <Icons.success className='mr-2 h-6 w-6' />
+      )}
+    </Button>
+    ) :(
+      <Button disabled={VerifyLoading}>
+        <Link href='/signup'>
+           <span> Please try to Create new Account</span>
+          </Link>
         </Button>
-      </form>
+    )
   );
 }
