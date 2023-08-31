848ae222003c81fa90d08829ef90d1383606a63b
MushfiqurRahmanTanim <smtanimurrahman404@gmail.com>
2023-08-31 14:39:15 +0600

    feat: :hammer: add loading page

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,31 +1,41 @@
-import GradientLogo from "@/components/common/shared/gradient-logo"
-import { AspectRatio } from "@/components/ui/aspect-ratio"
-import Image from "next/image"
-import Link from "next/link"
-
+import GradientLogo from '@/components/common/shared/gradient-logo';
+import { AspectRatio } from '@/components/ui/aspect-ratio';
+import Image from 'next/image';
+import Link from 'next/link';
 
 interface AuthLayoutProps {
-  children: React.ReactNode
+  children: React.ReactNode;
 }
 
 export default function AuthLayout({ children }: AuthLayoutProps) {
   return (
-    <div className="grid min-h-screen grid-cols-1 overflow-hidden md:grid-cols-3 lg:grid-cols-2">
+    <div className='grid min-h-screen grid-cols-1 overflow-hidden md:grid-cols-3 lg:grid-cols-2'>
       <AspectRatio ratio={16 / 9}>
-      <GradientLogo/>
-        <div className="absolute inset-0 bg-gradient-to-t from-background to-background/60 md:to-background/40" />
+        <Image
+          src='/login.jpg'
+          alt='A skateboarder doing a high drop'
+          fill
+          className='absolute inset-0 object-cover'
+          priority
+          sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
+        />
+        <GradientLogo />
+        <div className='absolute inset-0 bg-gradient-to-t from-background to-background/60 md:to-background/40' />
         <Link
-          href="/"
-          className="absolute left-8 top-6 z-20 flex items-center text-lg font-bold tracking-tight"
+          href='/'
+          className='absolute left-8 top-6 z-20 flex items-center text-lg font-bold tracking-tight'
         >
-           <Image src={'./zazila.svg'} alt="zazila-bazar" width={150} height={50}/>
-        
+          <Image
+            src={'./zazila.svg'}
+            alt='zazila-bazar'
+            width={150}
+            height={50}
+          />
         </Link>
-        
       </AspectRatio>
-      <main className="container absolute top-1/2 col-span-1 flex -translate-y-1/2 items-center md:static md:top-0 md:col-span-2 md:flex md:translate-y-0 lg:col-span-1">
+      <main className='container absolute top-1/2 col-span-1 flex -translate-y-1/2 items-center md:static md:top-0 md:col-span-2 md:flex md:translate-y-0 lg:col-span-1'>
         {children}
       </main>
     </div>
-  )
+  );
 }
--- src/app/(dashboard)/dashboard/account/page.tsx
+++ src/app/(dashboard)/dashboard/account/page.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import type { Metadata } from "next"
 
-const Account = () => {
-  return (
-    <div>Account</div>
-  )
+
+import { Shell } from "@/components/shells/shell"
+import { PageHeader, PageHeaderDescription, PageHeaderHeading } from "@/components/common/shared/page-header"
+
+export const metadata: Metadata = {
+  title: "Account",
+  description: "Manage your account settings",
 }
 
-export default Account
+export default function AccountPage() {
+  return (
+    <Shell variant="sidebar">
+      <PageHeader id="account-header" aria-labelledby="account-header-heading">
+        <PageHeaderHeading size="sm">Account</PageHeaderHeading>
+        <PageHeaderDescription size="sm">
+          Manage your account settings
+        </PageHeaderDescription>
+      </PageHeader>
+      <section
+        id="user-account-info"
+        aria-labelledby="user-account-info-heading"
+        className="w-full overflow-hidden"
+      >
+        helldjf
+      </section>
+    </Shell>
+  )
+}
