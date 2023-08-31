adb75b2e0279487f291b9c72e7ce433ea59ef2e1
MushfiqurRahmanTanim <smtanimurrahman404@gmail.com>
2023-08-31 14:43:09 +0600

    feat: :hammer: add auth layout logo

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -19,18 +19,12 @@ export default function AuthLayout({ children }: AuthLayoutProps) {
           priority
           sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
         />
-        <GradientLogo />
         <div className='absolute inset-0 bg-gradient-to-t from-background to-background/60 md:to-background/40' />
         <Link
           href='/'
           className='absolute left-8 top-6 z-20 flex items-center text-lg font-bold tracking-tight'
         >
-          <Image
-            src={'./zazila.svg'}
-            alt='zazila-bazar'
-            width={150}
-            height={50}
-          />
+          <GradientLogo />
         </Link>
       </AspectRatio>
       <main className='container absolute top-1/2 col-span-1 flex -translate-y-1/2 items-center md:static md:top-0 md:col-span-2 md:flex md:translate-y-0 lg:col-span-1'>
