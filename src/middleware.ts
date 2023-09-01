d003808d9a3e8a86f89c080212f63e5db789142d
MushfiqurRahmanTanim <smtanimurrahman404@gmail.com>
2023-09-01 17:44:26 +0600

    feat: :hammer: some changed

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,23 +3,23 @@ import { NextRequest } from 'next/server';
 import { ClientSession } from './configs/settings';
 
 export function middleware(req: NextRequest) {
-  const userToken = req.cookies.get('bazzar_sid')?.value;
-  const sessionToken = req.cookies.has(ClientSession as string);
+  const userToken = req.cookies.get(ClientSession as string)?.value;
+  // const sessionToken = req.cookies.has(ClientSession as string);
 
-  console.log(sessionToken);
+  console.log(userToken);
   console.log('USER TOKEN', userToken);
 
-  // console.log('ISSESSION', sessionToken);
+  // console.log('ISSESSION', userToken);
 
   const host = req.nextUrl.protocol + req.headers.get('host');
   // console.log("host", host);
 
   // user login control
-  if (sessionToken && req.nextUrl.pathname === '/signin') {
+  if (userToken && req.nextUrl.pathname === '/signin') {
     return NextResponse.redirect(new URL(`${host}/`));
   }
   // Add a closing bracket here
-  if (!sessionToken && req.nextUrl.pathname.includes('/dashboard')) {
+  if (!userToken && req.nextUrl.pathname.includes('/dashboard')) {
     return NextResponse.redirect(new URL(`${host}/signin`));
   }
 
