fe43a25c81d2e55d0b013c15359c58b49c00cfe8
SM Tanimur Rahman <smtanimurrahman404@gmail.com>
2023-10-16 14:31:33 +0600

    fix: :hammer: small bug fixed

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -27,9 +27,7 @@ interface DashboardLayoutProps {
   children: React.ReactNode;
 }
 
-export default async function DashboardLayout({
-  children,
-}: DashboardLayoutProps) {
+export default function DashboardLayout({ children }: DashboardLayoutProps) {
   // if (!user) {
   //   redirect('/signin');
   // }
