d268826e0ab0e80425ca15a490e0401982cb2c15
SMTanimur <smtanimurrahman404@gmail.com>
2023-11-09 21:19:47 +0600

    feat: :hammer: Search bar added

--- src/components/ui/search/search-box.tsx
+++ src/components/ui/search/search-box.tsx
@@ -13,12 +13,12 @@ export interface Props extends InputHTMLAttributes<HTMLInputElement> {
 
 const classes = {
   normal:
-    'bg-white pl-6 pr-6 pr-14 pl-14 rounded-tr-none rounded-tl-none rounded-br-none rounded-bl-none  border border-r-0 border-l-0 border-transparent focus:border-accent',
+    'bg-white dark:black/76  pl-6 pr-6 pr-14 pl-14 rounded-tr-none rounded-tl-none rounded-br-none rounded-bl-none  border border-r-0 border-l-0 border-transparent focus:border-accent',
   minimal:
-    'search-minimal bg-gray-100 pl-10 pr-10 pr-4 pl-4 md:pl-14 md:pr-14 border border-transparent focus:border-accent focus:bg-w',
-  flat: 'bg-white pl-10 pr-10 pr-4 pl-4 md:pl-14 md:pr-14 border-0',
+    'search-minimal dark:bg-gray-900 bg-gray-100 pl-10 pr-10 pr-4 pl-4 md:pl-14 md:pr-14 border border-transparent focus:border-accent focus:bg-gray-200 dark:focus:bg-gary-800',
+  flat: 'bg-white dark:black/76 pl-10 pr-10 pr-4 pl-4 md:pl-14 md:pr-14 border-0',
   'with-shadow':
-    'search-with-shadow bg-white pl-10 pr-10 pr-12 pl-12 md:pl-14 md:pr-14 focus:bg-white border-0',
+    'search-with-shadow bg-white dark:black/76 pl-10 pr-10 pr-12 pl-12 md:pl-14 md:pr-14 focus:bg-white border-0',
 };
 
 const SearchBox: React.FC<Props> = ({
@@ -51,7 +51,7 @@ const SearchBox: React.FC<Props> = ({
           value={value}
           autoComplete="off"
           className={cn(
-            'focus:ring-primary item-center flex h-full w-full appearance-none overflow-hidden truncate rounded-lg text-sm text-stone-800 placeholder-gray-500 transition duration-300 ease-in-out focus:outline-0 ',
+            'focus:ring-primary item-center flex h-full w-full appearance-none overflow-hidden truncate rounded-lg text-sm text-stone-800 placeholder-gray-500 transition duration-300 ease-in-out focus:outline-0 dark:text-gray-200',
             {
               'placeholder:text-slate-400': variant === 'flat',
             },
