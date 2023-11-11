9da4582351953ea53b10b1aae3c6449689a75a2f
SMTanimur <smtanimurrahman404@gmail.com>
2023-11-11 18:52:45 +0600

    fix: :hammer: fixed header bottom section

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -4,7 +4,9 @@ export default async function IndexPage() {
   return (
     <div className="">
       <Banner layout="classic"  variables={null}/>
-  
+      <h2>
+        lorem
+      </h2>
     </div>
   )
 }
--- src/components/layout/bottomFixedSection.tsx
+++ src/components/layout/bottomFixedSection.tsx
@@ -1,29 +1,56 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react'
 import { Button } from '../ui/button'
+import { ChevronUp } from 'lucide-react'
+import { cn } from '@/lib/utils'
 import { Icons } from '../ui/icons'
-import { ThemeSetting } from './ThemeSetting'
-import { ChevronDown } from 'lucide-react'
 
 const BottomFixedSection = () => {
+  const [showTopBtn, setShowTopBtn] = useState(false);
+  useEffect(() => {
+    const checkShowButtonScrollToTop = () => {
+      if (window.scrollY > 10) {
+        setShowTopBtn(true);
+      } else {
+        setShowTopBtn(false);
+      }
+    };
+    window.addEventListener("scroll", checkShowButtonScrollToTop);
+    return () => window.removeEventListener("scroll", checkShowButtonScrollToTop);
+  }, []);
+  const goToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  };
   return (
-    <div className='fixed bottom-10 right-16 flex gap-4 '>
-       <Button className='uppercase rounded-lg' >
+    <div className='fixed bottom-10 right-16 flex gap-6 '>
+       <Button className={cn('uppercase rounded-lg',!showTopBtn && 'mr-[52px]')} >
         Compare (3)
        </Button>
 
-       <div className=' relative flex-col  '>
+       <div className=' relative flex-col  gap-4'>
+          <Button 
+         
+          className={cn( 'flex justify-center items-center', showTopBtn ? 'block' : 'hidden') }
+        onClick={goToTop}
+      >
+        <ChevronUp className='w-5 text-center'/>
+       </Button>
+      
 
-       <div className='absolute bottom-14 right-0 lef'>
-          <Button>
-            djfkd
+<div className={cn(showTopBtn ? 'absolute bottom-14 right-0' : 'absolute bottom-0 right-0 ')}>
+          <Button
+         
+          >
+            <Icons.settings className='w-5'/>
           </Button>
        </div>
-
-       <Button className=''>
-        <ChevronDown className='w-5'/>
-       </Button>
+       
        
        </div>
+
        
 
     </div>
