c5aa1c7b3fd661a4f45f9c62f9b9bfa6b2b06015
SMTanimur <smtanimurrahman404@gmail.com>
2023-11-19 11:21:54 +0600

    refactor: :hammer: tailwindcss config change

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,14 +6,13 @@ module.exports = {
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     container: {
       center: true,
       padding: '2rem',
       screens: {
-        '2xl': '1500px',
+        '2xl': '1400px',
       },
     },
     extend: {
