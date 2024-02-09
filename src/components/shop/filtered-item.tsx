869dbb4f7bcd6de2be9ab37c3323931dbe9f0903
SMTanimur <smtanimurrahman404@gmail.com>
2024-02-09 21:45:32 +0600

    Change div to button in FilteredItem component

--- src/components/shop/filtered-item.tsx
+++ src/components/shop/filtered-item.tsx
@@ -9,13 +9,13 @@ interface Props {
 
 export const FilteredItem = ({ itemKey, itemValue, onClick }: Props) => {
   return (
-    <div
+    <button
       className="group flex shrink-0 m-1 items-center border border-border rounded-lg text-[13px] px-2.5 py-1.5 capitalize text-gray-600 dark:text-gray-100 cursor-pointer transition duration-200 ease-in-out hover:border-primary"
       role="button"
       onClick={onClick}
     >
       {itemKey && itemValue}
       <Icons.close className="text-sm text-gray-400 w-4  ml-2 shrink-0  mt-0.5 transition duration-200 ease-in-out group-hover:text-gray-900" />
-    </div>
+    </button>
   );
 };
